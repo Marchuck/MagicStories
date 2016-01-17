a7b0f17c9c697e4031aa13782622289b4a6afa09
VaccumMan <lukmar93@o2.pl>
2016-01-17 20:09:29 +0100

    hehe

--- app/src/main/java/pl/lukmarr/magicstories/MainActivity.java
+++ app/src/main/java/pl/lukmarr/magicstories/MainActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import pl.lukmarr.blueduff.BlueDuff;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BlueDuff blueDuff = new BlueDuff();
+
 
     }
 
